626c5294dd114f702b457bd5b40781e055efb9b9
hifromkevin <lam.kevin86@gmail.com>
2019-11-13 15:43:16 -0800

    Converts feedback number into star rating

--- src/FeedbackApp.js
+++ src/FeedbackApp.js
@@ -1,6 +1,20 @@
 module.exports = class FeedbackApp {
   formatFeedback(feedback) {
     /* Start here */
-    return `${feedback.word}: ${feedback.comment} ${feedback.rating} ${feedback.date}`;
+
+
+    // Convert feedback into star ratings
+    const starScale = (num, scale='') => {
+      if (num >= 15) {
+        return starScale(num - 20, scale+='★');
+      } else if (num >= 5) {
+        return scale+= '½';
+      } else {
+        return scale;
+      }
+    }
+
+
+    return `${feedback.word}: ${feedback.comment} ${starScale(feedback.rating)} ${feedback.date}`;
   }
 };
