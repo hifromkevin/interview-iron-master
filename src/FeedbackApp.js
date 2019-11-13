bcb2d955f169404b40c09050a6f028eb2f2f4590
hifromkevin <lam.kevin86@gmail.com>
2019-11-13 15:36:40 -0800

    Displays feedback in one line without any formatting

--- src/FeedbackApp.js
+++ src/FeedbackApp.js
@@ -1,5 +1,6 @@
 module.exports = class FeedbackApp {
   formatFeedback(feedback) {
     /* Start here */
+    return `${feedback.word}: ${feedback.comment} ${feedback.rating} ${feedback.date}`;
   }
 };
