b806858c4cf50a594cb10d5623c56812b3f88dfb
hifromkevin <lam.kevin86@gmail.com>
2019-11-13 18:52:05 -0800

    Cleans up recursive function call

--- src/library/commentLib.js
+++ src/library/commentLib.js
@@ -2,7 +2,7 @@
 // this helper will keep removing words until the sentence is less than the max, 
 // rather than cutting the comment mid-word
 let stopOnWord = (sentence, ratingLength, max=80, rotation=false) => {
-  return (sentence.length + ratingLength < (max - 4)) ? ((rotation) ? `${sentence} ...` : sentence) : stopOnWord(sentence.split(' ').slice(0,-1).join(' '), ratingLength, max, rotation = true);
+  return (sentence.length + ratingLength < (max - 4)) ? ((rotation) ? `${sentence} ...` : sentence) : stopOnWord(sentence.split(' ').slice(0,-1).join(' '), ratingLength, max, true);
 }
 
 exports.stopOnWord = stopOnWord;
