cf245122152bc87e637c266415fe63d11ff0b0b6
hifromkevin <lam.kevin86@gmail.com>
2019-11-13 16:13:58 -0800

    Breaks apart comment and gets length for each portion

--- src/FeedbackApp.js
+++ src/FeedbackApp.js
@@ -6,6 +6,16 @@ module.exports = class FeedbackApp {
     // Moved rating and library functions to library files to reduce code in formatFeedback function
     let lib = require('./library/index.js');
 
-    return `${feedback.word}: ${feedback.comment} ${lib.starScale(feedback.rating)} ${lib.formatDate(feedback.date)}`;
+    // Get the lengths of each block of text to anticipate 80 character maximum
+    let formattedDate = lib.formatDate(feedback.date);
+    let dateLength = formattedDate.length;
+
+    let rating = feedback.rating ? lib.starScale(feedback.rating) : '';
+    let ratingLength = rating.length;
+
+    let formattedFeedback = `${feedback.word}: ${feedback.comment}`;
+    let feedbackLength = formattedFeedback.length;
+
+    return `${formattedFeedback} ${rating} ${formattedDate}`;
   }
 };
