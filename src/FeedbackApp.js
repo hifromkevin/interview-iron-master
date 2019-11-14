b9eb1bfd667d3a82a69d29e7e0dd1bb43659417a
hifromkevin <lam.kevin86@gmail.com>
2019-11-13 18:00:05 -0800

    Cleans up spacing

--- src/FeedbackApp.js
+++ src/FeedbackApp.js
@@ -1,8 +1,6 @@
 module.exports = class FeedbackApp {
 
   formatFeedback(feedback) {
-    /* Start here */
-
     // Moved rating and library functions to library files to reduce code in formatFeedback function
     let lib = require('./library/index.js');
 
@@ -22,6 +20,6 @@ module.exports = class FeedbackApp {
       return rating ? `${formattedFeedback} ${rating} ${formattedDate}` : `${formattedFeedback} ${formattedDate}`;
     } else {
       return (feedbackLength + ratingLength > 79) ? `${formattedFeedback.split('').slice(0, (75 - ratingLength)).join('')}... ${rating}` : `${formattedFeedback} ${rating}`;
-    }
-  }
+    };
+  };
 };
