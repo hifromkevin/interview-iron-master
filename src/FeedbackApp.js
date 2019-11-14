8768d0036848676a4d141784f0ee16651961147f
hifromkevin <lam.kevin86@gmail.com>
2019-11-13 16:08:15 -0800

    Cleans up formatFeedback function

--- src/FeedbackApp.js
+++ src/FeedbackApp.js
@@ -2,6 +2,8 @@ module.exports = class FeedbackApp {
 
   formatFeedback(feedback) {
     /* Start here */
+
+    // Moved rating and library functions to library files to reduce code in formatFeedback function
     let lib = require('./library/index.js');
 
     return `${feedback.word}: ${feedback.comment} ${lib.starScale(feedback.rating)} ${lib.formatDate(feedback.date)}`;
