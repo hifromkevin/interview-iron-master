881625c7e38f1955606e7735b2c1fefaa49694da
hifromkevin <lam.kevin86@gmail.com>
2019-11-13 18:50:51 -0800

    Relocates comment truncation to another file

---
+++ src/library/commentLib.js
@@ -0,0 +1,8 @@
+// Rather than strictly stopping AT the max number of characters, 
+// this helper will keep removing words until the sentence is less than the max, 
+// rather than cutting the comment mid-word
+let stopOnWord = (sentence, ratingLength, max=80, rotation=false) => {
+  return (sentence.length + ratingLength < (max - 4)) ? ((rotation) ? `${sentence} ...` : sentence) : stopOnWord(sentence.split(' ').slice(0,-1).join(' '), ratingLength, max, rotation = true);
+}
+
+exports.stopOnWord = stopOnWord;
